08b6ed3eccc1df803d27a8f63a7d619f4246de07
Nidhogg <dbelenkiy@icloud.com>
2023-09-05 19:52:48 +0300

    Initial commit

--- src/dbanxter/main.mo
+++ src/dbanxter/main.mo
@@ -1,5 +1,49 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBanxter {
+  stable var currentValue: Float = 500;
+   currentValue := 500;
+
+  stable var startTime = Time.now();
+  startTime := Time.now();
+  Debug.print(debug_show(startTime));
+
+  let id = 7856383658;
+
+  // Debug.print(debug_show(id));
+
+public func topUp(amount: Float) {
+   currentValue += amount;
+   Debug.print(debug_show(currentValue));
+
   };
+
+public func withdrawl(amount: Float) {
+   let tempValue: Float = currentValue - amount;
+   if (tempValue >= 0) { 
+   currentValue -= amount;
+
+   Debug.print(debug_show(currentValue));
+  } else {
+    Debug.print("Amount too large, currentValue less than zero")
+  }
 };
+
+public query func checkBalance(): async Float {
+  return currentValue;
+};
+
+  // topUp();
+
+public func compound()  {
+   let currentTime = Time.now();
+   let timeElapsedNS = currentTime - startTime;
+   let timeElapsedS = timeElapsedNS / 1000000000;
+   currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+   startTime := currentTime;
+ }
+
+}
--- src/dbanxter_assets/src/index.js
+++ src/dbanxter_assets/src/index.js
@@ -1,19 +1,48 @@
 import { dbanxter } from "../../declarations/dbanxter";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbanxter.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async function() {
+   const currentAmount = await dbanxter.checkBalance();
+   const formattedAmount = parseFloat(currentAmount).toFixed(2);
+   document.getElementById("value").innerText = formattedAmount;
 });
+
+document.querySelector("form").addEventListener("submit", async function(event) {
+   event.preventDefault(); 
+   //console.log("Submitted.");
+
+   const button = event.target.querySelector("#submit-btn");
+    
+   const inputAmount = parseFloat(document.getElementById("input-amount").value);
+
+   button.setAttribute("disabled", true);
+
+   if (document.getElementById("input-amount").value.length != 0) {
+      await dbanxter.topUp(inputAmount);
+   }
+   
+   if (document.getElementById("withdraw-amount").value.length == 0) {
+       await dbanxter.withdraw(outputAmount);
+   }
+
+   const currentAmount = await dbanxter.checkBalance();
+   const formattedAmount = parseFloat(currentAmount).toFixed(2);
+   document.getElementById("value").innerText = formattedAmount;
+       
+   document.getElementById("input-amount").value = "";
+   document.getElementById("withdraw-amount").value = "";
+
+   await dbanxter.compound();
+   update();
+
+   
+   button.removeAttribute("disabled");
+});
+
+
+async function update() {
+   const currentAmount = await dbanxter.checkBalance();
+   const formattedAmount = parseFloat(currentAmount).toFixed(2);
+   document.getElementById("value").innerText = formattedAmount;
+
+
+}
